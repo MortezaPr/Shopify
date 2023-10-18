53d06cddcfc7d538ef88e25251a4755ae6fadde3
Morteza Pourramzan <pourramzanmorteza@gmail.com>
2023-10-18 16:51:29 +0330

    update docker files

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -4,12 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
-    },
+  preview: {
     host: true,
-    strictPort: true,
     port: 5173,
   },
 });
