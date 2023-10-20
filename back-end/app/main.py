3f670a62610522eb69d317fc548ae2d4c28cebfc
Arian-gh24 <arianghaharpour82@gmail.com>
2023-10-20 12:28:46 +0330

    set dockerfile

---
+++ back-end/app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
--- back-end/main.py
+++
@@ -1,3 +0,0 @@
-from fastapi import FastAPI
-from typing import Optional
-from pydantic import BaseModel
