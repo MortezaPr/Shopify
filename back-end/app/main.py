36002a9a6c372c94e103435835c4999eea0aff8b
Arian-gh24 <arianghaharpour82@gmail.com>
2023-10-23 18:30:20 +0330

    fix docker compose

--- back-end/app/main.py
+++ back-end/app/main.py
@@ -12,5 +12,5 @@ def read_root():
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+def read_item(item_id: int, q):
     return {"item_id": item_id, "q": q}
