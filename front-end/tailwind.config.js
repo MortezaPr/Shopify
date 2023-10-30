fd56eb3651ff2d840115f761ab40e6c8fc1ef5a0
Morteza Pourramzan <pourramzanmorteza@gmail.com>
2023-10-30 10:35:42 +0330

    update tailwind config

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -7,6 +7,41 @@ export default {
         "my-purple": "#865DFF",
       },
     },
+    keyframes: {
+      "slide-in": {
+        "0%": {
+          "-webkit-transform": "translateX(-200px)",
+          transform: "translateX(200px)",
+        },
+        "100%": {
+          "-webkit-transform": "translateX(0px)",
+          transform: "translateX(0px)",
+        },
+      },
+
+      "slide-fwd": {
+        "0%": {
+          "-webkit-transform": "translateZ(0px)",
+          transform: "translateZ(0px)",
+        },
+        "100%": {
+          "-webkit-transform": "translateZ(160px)",
+          transform: "translateZ(160px)",
+        },
+      },
+    },
+    animation: {
+      "slide-in": "slide-in 0.5s ease-out",
+      "slide-fwd":
+        " slide-fwd 0.45s cubic-bezier(0.250, 0.460, 0.450, 0.940) both",
+    },
+    transitionProperty: {
+      height: "height",
+    },
+    cursor: {
+      "zoom-in": "zoom-in",
+      pointer: "pointer",
+    },
   },
   plugins: [],
 };
