042e5d85c7f5244826364f4e8be222f36818f8c9
Morteza Pourramzan <pourramzanmorteza@gmail.com>
2024-01-08 01:50:08 +0330

    update tests

--- front-end/__tests__/ShoppingCart.test.tsx
+++ front-end/__tests__/ShoppingCart.test.tsx
@@ -1,37 +1,39 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import ShoppingCart from "@/app/shoppingCart/page";
+import Product from "@/app/product/[slug]/page";
 import "@testing-library/jest-dom";
 
 describe("ShoppingCart", () => {
-  it("Should remove item when button is clicked", () => {
-    // Arrange
-    render(<ShoppingCart />);
+  it("Should add item to cart when button is clicked in Product component", () => {
+    render(<Product />);
 
-    // Act
-    const button = screen.getByTestId("remove-button");
+    const button = screen.getByTestId("add-to-cart-button");
     fireEvent.click(button);
-
-    // Assert
-    expect(
-      screen.queryByText(
-        "گوشی موبایل اپل مدل آیفون 13 پرو مکس نات اکتیو TH/A (A2643) تک سیم کارت ظرفیت 256 گیگابایت رم 6 گیگابایت"
-      )
-    ).not.toBeInTheDocument();
   });
 
   it("Should increment count when button is clicked", () => {
-    // Arrange
     render(<ShoppingCart />);
 
-    // Act
     const button = screen.getByTestId("increment-button");
     const countElement = screen.getByTestId("item-count");
     const initialCount = Number(countElement.textContent);
 
     fireEvent.click(button);
 
-    // Assert
     const finalCount = Number(countElement.textContent);
     expect(finalCount).toBe(initialCount + 1);
+  });
+
+  it("Should remove item when button is clicked", () => {
+    render(<ShoppingCart />);
+
+    const button = screen.getByTestId("remove-button");
+    fireEvent.click(button);
+
+    expect(
+      screen.queryByText(
+        "گوشی موبایل اپل مدل آیفون 13 پرو مکس نات اکتیو TH/A (A2643) تک سیم کارت ظرفیت 256 گیگابایت رم 6 گیگابایت"
+      )
+    ).not.toBeInTheDocument();
   });
 });
