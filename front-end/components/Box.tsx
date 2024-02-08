57147897916bed4e58f040183d7c6a7f6958ac3d
Morteza Pourrmazan <pourramzanmorteza@gmail.com>
2024-02-08 11:37:35 +0330

    fix a bug in Box component

--- front-end/components/Box.tsx
+++ front-end/components/Box.tsx
@@ -62,35 +62,33 @@ const Card: React.FC<CardProps> = ({ title, items }) => {
             ref={scrollContainer}
           >
             {items.map((item, index) => (
-              <>
-                <Link
-                  href={`/product/${item.slug}`}
-                  className="mt-10 mr-10 flex"
-                  key={item.id}
-                >
-                  <div className="w-64 h-[19rem] border-slate-200 dark:border-neutral-800 border-2 rounded-lg">
-                    <div className="flex justify-center pt-2">
-                      <Image
-                        src={item.image}
-                        alt="image"
-                        width={160}
-                        height={160}
-                      />
-                    </div>
-                    <p className="pt-5 px-7 text-sm max-w-[20rem] overflow-hidden line-clamp-2">
-                      {item.name}
-                    </p>
-                    <p className="flex justify-center text-sm pt-4">
-                      {item.price.toLocaleString("fa")} تومان
-                    </p>
+              <Link
+                href={`/product/${item.slug}`}
+                className="mt-10 mr-10 flex"
+                key={item.id}
+              >
+                <div className="w-64 h-[19rem] border-slate-200 dark:border-neutral-800 border-2 rounded-lg">
+                  <div className="flex justify-center pt-2">
+                    <Image
+                      src={item.image}
+                      alt="image"
+                      width={160}
+                      height={160}
+                    />
                   </div>
-                </Link>
+                  <p className="pt-5 px-7 text-sm max-w-[20rem] overflow-hidden line-clamp-2">
+                    {item.name}
+                  </p>
+                  <p className="flex justify-center text-sm pt-4">
+                    {item.price.toLocaleString("fa")} تومان
+                  </p>
+                </div>
                 {index !== items.length - 1 ? (
                   <div className="h-[19rem] p-[1px] bg-slate-200 dark:bg-neutral-800 mr-10 mt-10"></div>
                 ) : (
                   <div className="ml-10"></div>
                 )}
-              </>
+              </Link>
             ))}
           </div>
           <button
