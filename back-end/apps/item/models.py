e92f58d24461ddacc3207ed71f5f20e557f28fc6
Morteza Pourrmazan <pourramzanmorteza@gmail.com>
2024-03-11 00:55:38 +0330

    Remove laptop and mobile description fields, add item description field

--- back-end/apps/item/models.py
+++ back-end/apps/item/models.py
@@ -6,6 +6,7 @@ class Item(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=False)
     image = models.ImageField(upload_to="item_image", null=True, blank=True)
     slug = models.SlugField(unique=True)
+    description = models.TextField(max_length=128, null=True, blank=True)
 
     def save(self, *args, **kwargs):
         if not self.slug:
@@ -17,11 +18,9 @@ class Mobile(Item):
     screen_tech = models.TextField(max_length=128, null=True, blank=True)
     os_version = models.CharField(max_length=128, null=True, blank=True)
     size = models.DecimalField(max_digits=3, decimal_places=1, null=True, blank=True)
-    description = models.TextField(max_length=128, null=True, blank=True)
     
 class Laptop(Item):
     processor = models.CharField(max_length=32, null=True, blank=True)
     ram = models.CharField(max_length=32, null=True, blank=True)
     internal_memory = models.CharField(max_length=32, null=True, blank=True)
     GPU = models.CharField(max_length=32, null=True, blank=True)
-    description = models.TextField(max_length=128, null=True, blank=True)
