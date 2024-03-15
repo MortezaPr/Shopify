9e7c57355012563d4c03903035b2e239c370429b
Morteza Pourrmazan <pourramzanmorteza@gmail.com>
2024-03-15 15:47:53 +0330

    Add verify user view to handle otp verification

--- back-end/apps/users/serializers.py
+++ back-end/apps/users/serializers.py
@@ -1,8 +1,11 @@
-import uuid
-from rest_framework import serializers
-from .models import Customer, User
-import redis
 import random
+import uuid
+
+import redis
+from django.conf import settings
+from rest_framework import serializers
+
+from .models import Customer, User
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -12,7 +15,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     # set_password use to save password hashed in db
     def create(self, validate_data):
-       # Generate a unique username
+        # Generate a unique username
         username = uuid.uuid4().hex
 
         user = User(username=username)
@@ -21,33 +24,34 @@ class UserSerializer(serializers.ModelSerializer):
         customer = Customer(user=user, phone_number=validate_data["phone_number"])
         customer.save()
 
-        # # Create a Redis connection
-        # r = redis.Redis(host='localhost', port=6379, db=0)
+        # Create a Redis connection
+        r = redis.Redis(
+            host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB
+        )
 
-        # # Generate a 6-digit OTP
-        # otp = random.randint(100000, 999999)
+        # Generate a 6-digit OTP
+        otp = random.randint(100000, 999999)
 
-        # # Store the OTP in Redis, associated with the user's phone number
-        # # Set it to expire after 60 seconds
-        # r.setex(customer.phone_number, 60, otp)
+        # Store the OTP in Redis, associated with the user's phone number
+        # Set it to expire after 60 seconds
+        r.setex(customer.phone_number, 60, otp)
 
-        # print(f"Your OTP is: {otp}")
-        
+        print(f"Your OTP is: {otp}")
+
         return customer
-    
-    # def update(self, instance, validated_data):
-    #     if "email" in validated_data:
-    #         instance.email = validated_data.get("email", instance.email)
-        
-    #     if "first_name" in validated_data:
-    #         instance.first_name = validated_data.get("first_name", instance.first_name)
-        
-    #     if "last_name" in validated_data:
-    #         instance.last_name = validated_data.get("last_name", instance.last_name)
-            
-    #     if "password" in validated_data:
-    #         instance.set_password(validated_data["password"])
-        
-    #     instance.save()
-    #     return instance
-        
+
+    def update(self, instance, validated_data):
+        if "email" in validated_data:
+            instance.email = validated_data.get("email", instance.email)
+
+        if "first_name" in validated_data:
+            instance.first_name = validated_data.get("first_name", instance.first_name)
+
+        if "last_name" in validated_data:
+            instance.last_name = validated_data.get("last_name", instance.last_name)
+
+        if "password" in validated_data:
+            instance.set_password(validated_data["password"])
+
+        instance.save()
+        return instance
--- back-end/apps/users/views.py
+++ back-end/apps/users/views.py
@@ -1,29 +1,39 @@
-from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, DestroyAPIView, UpdateAPIView
-from rest_framework.permissions import IsAdminUser
-
-from .serializers import UserSerializer
-from .models import User
-
+from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import JsonResponse
+from redis import Redis
+from rest_framework.generics import (
+    CreateAPIView,
+    DestroyAPIView,
+    ListAPIView,
+    RetrieveAPIView,
+    UpdateAPIView,
+)
+from rest_framework.permissions import AllowAny, IsAdminUser
 from rest_framework.views import APIView
-from rest_framework.permissions import AllowAny
-from .models import Customer
+
+from .models import Customer, User
+from .serializers import UserSerializer
+
 
 class CreateUserView(CreateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
     permission_classes = []
-    
+
+
 class UserListView(ListAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
     permission_classes = [IsAdminUser]
-    
+
+
 class UserDetailView(RetrieveAPIView):
     lookup_field = "id"
     serializer_class = UserSerializer
     queryset = User.objects.all()
     permission_classes = [IsAdminUser]
+
 
 class UpdateUserView(UpdateAPIView):
     lookup_field = "id"
@@ -31,15 +41,53 @@ class UpdateUserView(UpdateAPIView):
     queryset = User.objects.all()
     permission_classes = [IsAdminUser]
 
+
 class DeleteUserView(DestroyAPIView):
     lookup_field = "id"
     serializer_class = UserSerializer
     queryset = User.objects.all()
-    permission_classes = [IsAdminUser]    
+    permission_classes = [IsAdminUser]
 
-class CheckUserView(APIView):
+
+class CheckUserStatusView(APIView):
     permission_classes = [AllowAny]
 
-    def get(self, request, phone_number, format=None):
+    def get(self, request, phone_number):
         exists = Customer.objects.filter(phone_number=phone_number).exists()
-        return JsonResponse({'exists': exists})
+        is_verified = False
+        if exists:
+            customer = Customer.objects.get(phone_number=phone_number)
+            is_verified = customer.is_verified
+        return JsonResponse({"exists": exists, "is_verified": is_verified})
+
+
+class VerifyUserView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        phone_number = request.data.get("phone_number")
+        otp = request.data.get("otp")
+
+        print(type(otp))
+
+        # Connect to Redis
+        redis_conn = Redis(
+            host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB
+        )
+
+        # Get the OTP stored in Redis
+        redis_otp = redis_conn.get(phone_number)
+        print(type(redis_otp))
+        print(f"this is otp stored in redis {redis_otp}")
+
+        if redis_otp is not None and otp == redis_otp.decode():
+            try:
+                # If OTP is correct, set is_verified to True
+                customer = Customer.objects.get(phone_number=phone_number)
+                customer.is_verified = True
+                customer.save()
+                return JsonResponse({"status": "verified"})
+            except ObjectDoesNotExist:
+                return JsonResponse({"error": "Customer does not exist"}, status=400)
+        else:
+            return JsonResponse({"error": "Invalid OTP"}, status=400)
