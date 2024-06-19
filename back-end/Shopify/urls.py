19f7c443498361c85f14332ec2b3571e626f395a
Morteza Pourrmazan <pourramzanmorteza@gmail.com>
2024-06-19 21:37:37 +0330

    chore: Update root URL for Swagger UI

--- back-end/Shopify/urls.py
+++ back-end/Shopify/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     path("store/product/", include("Store.Product.urls")),
     path("store/shopping-cart/", include("Store.Cart.urls")),
     path('docs/schema', SpectacularAPIView.as_view(), name='schema'),
-    path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
