0160258c78a95e03909f31429df190dcfd0bdce2
Morteza Pourrmazan <pourramzanmorteza@gmail.com>
2024-06-20 16:41:57 +0330

    Update image fetching logic in shopping cart page

--- front-end/app/shoppingCart/page.tsx
+++ front-end/app/shoppingCart/page.tsx
@@ -2,10 +2,10 @@
 import React from "react";
 import shopping from "@/public/images/shopping.png";
 import Image from "next/image";
-import iphone from "@/public/images/iphone.webp";
 import { FaRegTrashAlt } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
 import useCart from "@/hooks/useCart";
+import { Product } from "@/types/Product";
 
 const ShoppingCart = () => {
   const { items, addItem, editItem, removeItem } = useCart();
@@ -60,14 +60,19 @@ const ShoppingCart = () => {
       ) : (
         <div className="flex gap-5">
           <div className="flex flex-col gap-4">
-            {shopping_cart.map((item) => (
+            {shopping_cart.map((item: Product) => (
               <div
                 key={item.id}
                 className="h-72 w-screen flex justify-center lg:justify-normal lg:w-[40rem] xl:w-[50rem] border-2 border-gray-200 dark:border-neutral-800 bg-white dark:bg-dark-user rounded-lg"
               >
                 <div className="flex pt-14 lg:pr-8">
                   <div>
-                    <Image src={item.pic} width={150} height={150} alt="pic" />
+                    <Image
+                      src={item.image?.image_url}
+                      width={150}
+                      height={150}
+                      alt="pic"
+                    />
                   </div>
                   <div className="flex flex-col gap-4 pr-5 pl-5 lg:pl-0">
                     <p className="max-w-[30rem] overflow-hidden line-clamp-2 leading-7 text-sm font-bold">
