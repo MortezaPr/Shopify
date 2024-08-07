f2270153475027416f782418e8795a86ed0dfd87
Morteza Pourramzan <pourramzanmorteza@gmail.com>
2024-08-07 18:58:04 +0330

    feat: Add "Add to Cart" button for logged-in users in Products page

--- src/app/(website)/products/components/Products.tsx
+++ src/app/(website)/products/components/Products.tsx
@@ -1,6 +1,7 @@
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
@@ -8,12 +9,14 @@ import { getProducts } from "@/services/getProducts";
 import ProductImage from "./ProductImage";
 import { Product } from "@/types";
 import Link from "next/link";
+import { cookies } from "next/headers";
 
 export default async function Products({
   searchParams = {},
 }: {
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
+  const isLogedIn = cookies().has("token");
   const page = parseInt(searchParams["page"] as string, 10) || 1;
   const limit = 10;
   const products = await getProducts(page, limit);
@@ -24,7 +27,6 @@ export default async function Products({
         sx={{
           padding: 2,
           marginTop: 2,
-
           boxShadow: 1,
           borderRadius: 3,
           "@media (min-width:600px)": {
@@ -40,7 +42,12 @@ export default async function Products({
             <Grid item xs={12} sm={6} md={4} lg={3} key={product.id}>
               <Link href={`/products/${product.id}`}>
                 <Card
-                  sx={{ height: "100%", borderRadius: 2 }}
+                  sx={{
+                    height: "100%",
+                    borderRadius: 2,
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
                   className="cursor-pointer"
                 >
                   <div className="flex items-center justify-center h-48 mt-5 mb-2">
@@ -59,6 +66,13 @@ export default async function Products({
                     <Typography color="text.primary">
                       {product.price.toLocaleString("fa")}
                     </Typography>
+                  </CardContent>
+                  <CardContent sx={{ mt: "auto" }}>
+                    {isLogedIn && (
+                      <Button variant="outlined" color="primary" fullWidth>
+                        اضافه کردن به سبد خرید
+                      </Button>
+                    )}
                   </CardContent>
                 </Card>
               </Link>
